252b3c6819462bbfa71cdd79f4bf627459e3eb83
ernur <ernurtorekul@gmail.com>
2025-09-13 11:28:05 +0500

    fixed gitignore

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("35ff15144dd3290b")
+/******/ 		__webpack_require__.h = () => ("e230717509976f48")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
